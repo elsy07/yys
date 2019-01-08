bfcb3132cbe22b312edb5521fefd445e1a17b53b
elsy07 <xux001@gmail.com>
2019-01-08 21:27:58 +0800

    碎片登记页完成

--- pages/asset/asset.js
+++ pages/asset/asset.js
@@ -7,18 +7,18 @@ Page({
     num: 0,
     canEx: true,
     canSell: false,
+    canBuy: false,
     activeIndex: 'ssr',
     type: ['ssr', 'sp', '联动'],
-    shikigami: {
-      'ssr': [],
-      'sp': [],
-      '联动': []
-    },
+    shikigami: {},
     shikigamiByType: [],
-    myAssets: []
+    shikigamiAll: [],
+    myAssets: [],
+    want: '315',
+    wantIndex: 0
   },
 
-  getData: function () {
+  getData: function() {
     console.log('getData')
 
     wx.showLoading({
@@ -34,52 +34,50 @@ Page({
     let query = new wx.BaaS.Query()
     query.compare('created_by', '=', uid)
     Fragment.setQuery(query).select(['shikigami', 'num', 'canEx', 'canSell']).find().then(res => {
-      // success 
       console.log(res)
       this.data.myAssets = res.data.objects
 
-
+      //查找所有式神
       let shikigami = new wx.BaaS.TableObject(61369)
-      let ssr = new wx.BaaS.Query()
+      shikigami.limit(50).find().then(res => {
+        that.setData({
+          shikigamiAll: res.data.objects
+        })
 
-      //查找所有ssr 
-      ssr.compare('type', '=', 'ssr')
-      shikigami.setQuery(ssr).limit(50).find().then(res => {
-        // success 
-        that.data.shikigami.ssr = res.data.objects
+        var ssr = []
+        var sp = []
+        var ld = []
 
-        for (var i in that.data.shikigami.ssr) {
+        for (var i in that.data.shikigamiAll) {
           for (var j in that.data.myAssets) {
-            if (that.data.shikigami.ssr[i].num == that.data.myAssets[j].shikigami) {
-              that.data.shikigami.ssr[i].fragNum = that.data.myAssets[j].num
+            if (that.data.shikigamiAll[i].num == that.data.myAssets[j].shikigami) {
+              that.data.shikigamiAll[i].fragNum = that.data.myAssets[j].num
             }
           }
+          if (that.data.shikigamiAll[i].type == 'ssr') {
+            ssr.push(that.data.shikigamiAll[i])
+          }
+          if (that.data.shikigamiAll[i].type == 'sp') {
+            sp.push(that.data.shikigamiAll[i])
+          }
+          if (that.data.shikigamiAll[i].type == '联动') {
+            ld.push(that.data.shikigamiAll[i])
+          }
         }
+
+        that.setData({
+          shikigami: {
+            'ssr': ssr,
+            'sp': sp,
+            '联动': ld
+          }
+        })
+
         if (that.data.activeIndex == 'ssr') {
           that.setData({
             shikigamiByType: that.data.shikigami.ssr
           })
           wx.hideLoading()
-        }
-
-      }, err => {
-        // err 
-        console.error(err)
-      })
-
-
-      //查找所有sp 
-      let sp = new wx.BaaS.Query()
-      sp.compare('type', '=', 'sp')
-      shikigami.setQuery(sp).limit(50).find().then(res => {
-        // success 
-        that.data.shikigami.sp = res.data.objects
-        for (var i in that.data.shikigami.sp) {
-          for (var j in that.data.myAssets) {
-            if (that.data.shikigami.sp[i].num == that.data.myAssets[j].shikigami) {
-              that.data.shikigami.sp[i].fragNum = that.data.myAssets[j].num
-            }
-          }
         }
         if (that.data.activeIndex == 'sp') {
           that.setData({
@@ -87,34 +85,98 @@ Page({
           })
           wx.hideLoading()
         }
-      }, err => {
-        // err 
-        console.error(err)
-      })
-
-      //查找所有联动 
-      let ld = new wx.BaaS.Query()
-      ld.compare('type', '=', '联动')
-      shikigami.setQuery(ld).limit(50).find().then(res => {
-        // success 
-        that.data.shikigami['联动'] = res.data.objects
-        for (var i in that.data.shikigami['联动']) {
-          for (var j in that.data.myAssets) {
-            if (that.data.shikigami['联动'][i].num == that.data.myAssets[j].shikigami) {
-              that.data.shikigami['联动'][i].fragNum = that.data.myAssets[j].num
-            }
-          }
-        }
         if (that.data.activeIndex == '联动') {
           that.setData({
             shikigamiByType: that.data.shikigami['联动']
           })
           wx.hideLoading()
         }
+
       }, err => {
         // err 
         console.error(err)
       })
+
+
+      // //查找所有ssr 
+      // let ssr = new wx.BaaS.Query()
+      // ssr.compare('type', '=', 'ssr')
+      // shikigami.setQuery(ssr).limit(50).find().then(res => {
+      //   // success 
+      //   that.data.shikigami.ssr = res.data.objects
+      //   that.data.shikigamiAll.concat(res.data.objects)
+
+      //   for (var i in that.data.shikigami.ssr) {
+      //     for (var j in that.data.myAssets) {
+      //       if (that.data.shikigami.ssr[i].num == that.data.myAssets[j].shikigami) {
+      //         that.data.shikigami.ssr[i].fragNum = that.data.myAssets[j].num
+      //       }
+      //     }
+      //   }
+      //   if (that.data.activeIndex == 'ssr') {
+      //     that.setData({
+      //       shikigamiByType: that.data.shikigami.ssr
+      //     })
+      //     wx.hideLoading()
+      //   }
+
+      // }, err => {
+      //   // err 
+      //   console.error(err)
+      // })
+
+
+      // //查找所有sp 
+      // let sp = new wx.BaaS.Query()
+      // sp.compare('type', '=', 'sp')
+      // shikigami.setQuery(sp).limit(50).find().then(res => {
+      //   // success 
+      //   that.data.shikigami.sp = res.data.objects
+      //   that.data.shikigamiAll.concat(res.data.objects)
+
+      //   for (var i in that.data.shikigami.sp) {
+      //     for (var j in that.data.myAssets) {
+      //       if (that.data.shikigami.sp[i].num == that.data.myAssets[j].shikigami) {
+      //         that.data.shikigami.sp[i].fragNum = that.data.myAssets[j].num
+      //       }
+      //     }
+      //   }
+      //   if (that.data.activeIndex == 'sp') {
+      //     that.setData({
+      //       shikigamiByType: that.data.shikigami.sp
+      //     })
+      //     wx.hideLoading()
+      //   }
+      // }, err => {
+      //   // err 
+      //   console.error(err)
+      // })
+
+      // //查找所有联动 
+      // let ld = new wx.BaaS.Query()
+      // ld.compare('type', '=', '联动')
+      // shikigami.setQuery(ld).limit(50).find().then(res => {
+      //   // success 
+      //   that.data.shikigami['联动'] = res.data.objects
+      //   that.data.shikigamiAll.concat(res.data.objects)
+
+      //   for (var i in that.data.shikigami['联动']) {
+      //     for (var j in that.data.myAssets) {
+      //       if (that.data.shikigami['联动'][i].num == that.data.myAssets[j].shikigami) {
+      //         that.data.shikigami['联动'][i].fragNum = that.data.myAssets[j].num
+      //       }
+      //     }
+      //   }
+      //   if (that.data.activeIndex == '联动') {
+      //     that.setData({
+      //       shikigamiByType: that.data.shikigami['联动']
+      //     })
+      //     wx.hideLoading()
+      //   }
+      // }, err => {
+      //   // err 
+      //   console.error(err)
+      // })
 
 
     }), err => {
@@ -127,7 +189,7 @@ Page({
   },
 
   //式神稀有度tab切换 
-  changeActive: function (e) {
+  changeActive: function(e) {
     console.log(e)
     var type = e.target.id
 
@@ -137,8 +199,66 @@ Page({
     });
   },
 
+  formSubmit: function(e) {
+    console.log(e)
+    var that = this
+    let Membership = new wx.BaaS.TableObject('membership')
+    var User = new wx.BaaS.User()
+    let uid = wx.BaaS.storage.get("uid")
+    let query = new wx.BaaS.Query()
+    query.compare('user', '=', User.getWithoutData(uid))
+    Membership.setQuery(query).find().then(res => {
+      console.log(res)
+      if(res.data.meta.total_count){
+        let recordID = res.data.objects[0].id
+        let member = Membership.getWithoutData(recordID)
+        member.set({
+          canBuy: that.data.canBuy,
+          want: that.data.want
+        })
+        member.update().then(res => {
+          // success
+          wx.showToast({
+            title: '已保存',
+            icon: 'success',
+            duration: 2000
+          })
+        }, err => {
+          // err
+          console.log(err)
+          wx.showToast({
+            title: '失败',
+            icon: 'none',
+            duration: 2000
+          })
+        })
+      }
+    }, err => {
+      // err
+      console.log(err)
+      wx.showToast({
+        title: '没有该用户',
+        icon: 'none'
+      })
+    })
+  },
+
+  onChangeWant: function(e) {
+    console.log(e)
+    this.setData({
+      want: this.data.shikigamiAll[e.detail.value].num,
+      wantIndex: e.detail.value
+    })
+  },
+  onChangePY: function (e) {
+    console.log(e)
+    this.setData({
+      canBuy: !this.data.canBuy
+    })
+  },
+
   //点击头像开始登记碎片 
-  regAsset: function (e) {
+  regAsset: function(e) {
     console.log(e)
     var shikigamiID = e.currentTarget.id
     this.setData({
@@ -151,7 +271,7 @@ Page({
   },
 
   //登记数量 
-  onChangeNum: function (e) {
+  onChangeNum: function(e) {
     console.log(e)
     this.setData({
       num: e.detail.value
@@ -159,7 +279,7 @@ Page({
   },
 
   //是否交换 
-  onChangeEx: function (e) {
+  onChangeEx: function(e) {
     console.log(e)
     this.setData({
       canEx: e.detail.value
@@ -167,7 +287,7 @@ Page({
   },
 
   //是否买卖 
-  onChangeSell: function (e) {
+  onChangeSell: function(e) {
     console.log(e)
     this.setData({
       canSell: e.detail.value
@@ -175,7 +295,7 @@ Page({
   },
 
   //登记确认 
-  modalConfirm: function (e) {
+  modalConfirm: function(e) {
     console.log(e)
     var that = this
     //登记
@@ -199,7 +319,7 @@ Page({
           canSell: this.data.canSell
         })
         asset.save().then(res => {
-  
+
           this.setData({
             showModal: false,
           })
@@ -240,6 +360,7 @@ Page({
           })
         })
       }
+      this.getData()
     }, err => {
       // err
       console.log(err)
@@ -247,11 +368,11 @@ Page({
         title: 'err',
       })
     })
-    
+
   },
 
   //登记取消 
-  modalCancel: function (e) {
+  modalCancel: function(e) {
     console.log(e)
     this.setData({
       showModal: false
@@ -259,7 +380,7 @@ Page({
   },
 
 
-  onLoad: function (a) {
+  onLoad: function(a) {
     var that = this;
     this.getData()
   },

7b680250f1926714f06694defa31b1d1f69c4bc3
elsy07 <xux001@gmail.com>
2019-01-09 15:08:24 +0800

    wxParser改插件实现

--- pages/content/content.js
+++ pages/content/content.js
@@ -1,4 +1,4 @@
-const wxParser = require('../../wxParser/index')
+// const wxParser = require('../../wxParser/index')
 
 Page({
 
@@ -6,91 +6,99 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    richText: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    wx.showLoading({
+      title: '大姐正在输入',
+    })
     var that = this
     let id = options.id
     let groupid = options.groupid
 
     let ContentGroupTest = new wx.BaaS.ContentGroup(groupid)
     ContentGroupTest.getContent(id).then(res => {
-      // success
       console.log(res.data)
       if (res.data) {
         let html = res.data.content
-
-        wxParser.parse({
-          bind: 'richText',
-          html: html,
-          target: that,
-          enablePreviewImage: true, // 禁用图片预览功能
-          tapLink: (url) => { // 点击超链接时的回调函数
-            // url 就是 HTML 富文本中 a 标签的 href 属性值
-            // 这里可以自定义点击事件逻辑，比如页面跳转
-            wx.navigateTo({
-              url
-            })
-          }
+        that.setData({
+          richText: html
         })
+        wx.hideLoading()
+        // wxParser.parse({
+        //   bind: 'richText',
+        //   html: html,
+        //   target: that,
+        //   enablePreviewImage: true, // 禁用图片预览功能
+        //   tapLink: (url) => { // 点击超链接时的回调函数
+        //     // url 就是 HTML 富文本中 a 标签的 href 属性值
+        //     // 这里可以自定义点击事件逻辑，比如页面跳转
+        //     wx.navigateTo({
+        //       url
+        //     })
+        //   }
+        // })
       }
 
     }, err => {
-      // err
+      console.log(err)
+      wx.showToast({
+        title: '内容获取失败',
+        icon: 'none'
+      })
     })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/test/test.js
+++ pages/test/test.js
@@ -1,41 +1,47 @@
-const wxParser = require('../../wxParser/index')
+// const wxParser = require('../../wxParser/index')
 
 Page({
   data: {
-    contents:{}
+    contents: {}
   },
 
-onClick: function(e){
-  console.log(e.target)
-  wx.navigateTo({
-    url: '/pages/testpage/testpage?id=' + e.target.id + "&groupid=" + e.target.dataset.groupid,
-  })
-
-},
-
-  onLoad: function (options) {
-    let that = this
-    let groupid = options.groupid
-    let categoryid = parseInt(options.categoryid) 
-    console.log(options)
-    let query = new wx.BaaS.Query()
-    query.arrayContains('categories', [categoryid])
-    let ContentGroupTest = new wx.BaaS.ContentGroup(groupid)
-
-    ContentGroupTest.setQuery(query).find().then(res => {
-      // success
-      console.log(res.data)
-      that.setData({
-        contents: res.data.objects
-      })
-    }, err => {
-      // err
-      console.log(err)
+  onClick: function(e) {
+    console.log(e.target)
+    wx.navigateTo({
+      url: '/pages/testpage/testpage?id=' + e.target.id + "&groupid=" + e.target.dataset.groupid,
     })
 
+  },
+
+  onLoad: function(options) {
+    let that = this
+    console.log(options)
+    let groupid = options.groupid
     
+    let contentid = parseInt(options.contentid)
+
+    if (contentid) {
+      let query = new wx.BaaS.Query()
+      query.arrayContains('id', [categoryid])
+      let ContentGroupTest = new wx.BaaS.ContentGroup(groupid)
+
+      ContentGroupTest.setQuery(query).find().then(res => {
+        // success
+        console.log(res.data)
+        that.setData({
+          contents: res.data.objects
+        })
+      }, err => {
+        // err
+        console.log(err)
+      })
+    }
 
 
-    
+
+
+
+
+
   }
 })
